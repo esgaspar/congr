5ef216db5d128e69abb4da4c4570b8acd54e1555
Erick Souza Gaspar <erickgaspar@sisconsultoria.com.br>
2019-10-16 11:10:01 -0300

    ajustes de db e scrapp

--- lib/meeting/meetingModel.ts
+++ lib/meeting/meetingModel.ts
@@ -1,15 +1,42 @@
-import * as mongoose from 'mongoose';
+import * as mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 export const MeetingSchema = new Schema({
-    day: { type: Date },
-    date: { type: Date },
-    hour: { type: Date },
-    meetingType: { type: String },
-    congregation: { congragationId: { type: String } },
-    designations: [
-        { designationId: { type: String } }
-    ],
-    created_date: { type: Date, default: Date.now }
+  day: { type: Date },
+  date: { type: Date },
+  hour: { type: Date },
+  meetingType: { type: String },
+  congregation: { congragationId: { type: String } },
+  designations: [
+    {
+      title: { type: String, required: "Enter a title designation" },
+      type: { type: String },
+      time: { type: String },
+      temp: { type: String },
+      study: {
+        title: { type: String },
+        conclued: { type: Boolean },
+        studyPoint: { type: String },
+        conclusionDate: { type: Date },
+        usedTime: { type: Date },
+        commments: { type: String }
+      },
+      date: { type: Date },
+      privilege: { type: String },
+      designed: {
+        substitution: { type: Boolean },
+        responsible: { contactId: { type: String } },
+        substitute: { contactId: { type: String } }
+      },
+      assistant: {
+        substitution: { type: Boolean },
+        responsible: { contactId: { type: String } },
+        substitute: { contactId: { type: String } }
+      },
+      meeting: { meetingId: { type: String } },
+      created_date: { type: Date, default: Date.now }
+    }
+  ],
+  created_date: { type: Date, default: Date.now }
 });

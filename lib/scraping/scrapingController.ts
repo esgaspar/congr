a71c6bca597fdd29ee59d5bfb7117bf03fd11f11
Erick Souza Gaspar <erickgaspar@sisconsultoria.com.br>
2019-10-17 16:20:19 -0300

    scrapping

--- lib/scraping/scrapingController.ts
+++ lib/scraping/scrapingController.ts
@@ -12,94 +12,135 @@ const langs = [
 ];
 
 export class ScrapingController {
-
   public lang = { lang: "", langId: "", cod: "" };
   public meetings = [];
+  public position = 2;
 
-  constructor() {
-    console.log(this);
-  }
+  public pGroup =   ".pGroup ";
+  public midweek = ".docClass-106 "
+  public session1 = this.midweek + "#section1 ";
+  public session2 = this.midweek + "#section2 ";
+  public session3 = this.midweek + "#section3 ";
+  public session4 = this.midweek + "#section4 ";
 
-    static fillDesignation = async (type, tag, html, dateString) => {
+ 
+  constructor() {}
+
+  public fillDesignation = async (type, session, pGroup, tag, html, dateString) => {
     let designation = { date: "", title: "", type: "" };
     designation.type = type;
-    designation.title = cheerio(tag, html).text();
+    designation.title = cheerio(session + pGroup + tag, html).text();
     designation.date = dateString;
-    return designation;
+    return designation; 
   };
+  
+  private skipNoMarker(session, pGroup, tag, html){
+    return cheerio(session + pGroup + " .noMarker " +tag, html).text().length > 5
+  }
 
-    static addDesignation = async (meeting, designation) => {
-      if(designation && designation.title.length > 0){
-        meeting.designations.push(designation);
-      }
+  public addDesignation = async (meeting, designation, title,  session, pGroup, tag , html, dateString) => {
+
+    if (designation && designation.title.length > 5) {
+      designation.title = designation.title;
+      this.position++;
+      meeting.designations.push(designation);
+      return true;
+    }else if(title == 'gems'){
+      this.position++;
+      this.newDesignation(meeting, title, this.session2, this.pGroup , ` #p${this.position} > strong` , html, dateString);
+      this.position--;
+      return true;
+    }
+    return false;
   };
 
   public getScrapings = async (req: Request, res: Response) => {
     this.lang = langs.find(lang => lang.lang === req.query.lang);
     let date = req.query.date;
 
-    await this.scrap(date, this.meetings, req);
+    this.meetings = await this.scrap(date, this.meetings, req);
 
     res.send(this.meetings);
   };
 
+  public async newDesignation(meeting, title, session, pGroup, tag , html, dateString){
+    return await this.addDesignation(
+      meeting,
+      await this.fillDesignation(
+        title,
+        session, pGroup, 
+        tag,
+        html,
+        dateString
+      ),
+      title, session, pGroup, tag , html, dateString
+    );
+  }
+  
   private scrap = async (date: any, meetings, req) => {
-    
     let momentDate = moment(date, "YYYY/MM/DD");
-    //momentDate.subtract(3, "months");
+    //momentDatebtract(3, "months");
     let stop = false;
     while (!stop) {
-            let meeting = {
-              date: {},
-              meetingType: {},
-              designations: []
-            };
-      let dateString = momentDate.format("YYYY") + "/" + momentDate.format("MM") + "/" + momentDate.format("DD");
+      this.position = 2;
+      let meeting = {
+        date: {},
+        meetingType: {},
+        designations: []
+      };
+      let dateString = this.getStringDate(momentDate);
+
+      console.log(`importando - ${dateString}`);
       meeting.date = momentDate;
       meeting.meetingType = "Meio de semana";
       let adress = `https://wol.jw.org/${this.lang.lang}/wol/dt/${this.lang.cod}/${this.lang.langId}/${dateString}`;
       await rp(adress)
-        .then(async (html) => {
-
-          let weekReading = await ScrapingController.fillDesignation( "weekReading", "#p2 > a > strong", html, dateString);
-
-
-         
+        .then(async html => {
+          let weekReading = await this.fillDesignation( "weekReading", "", "",` #p${this.position} > a > strong`, html, dateString);
           if (!weekReading.title) {
             stop = true;
             return;
           }
-
-        try{
           meeting.designations.push(weekReading);
+          this.position++;
+          
+          try {
 
-          let pGroup = ".pGroup "
-          let session1= "#section1 "
+            await this.newDesignation(meeting, "initialSong", this.session1, this.pGroup , ` #p${this.position}` , html, dateString);
+            await this.newDesignation(meeting, "p", this.session1, this.pGroup , ` #p${this.position}` , html, dateString);
+            
+            await this.newDesignation(meeting, "treasuresTitle", this.session2 , "" ,` #p${this.position} > strong` , html, dateString);
+            await this.newDesignation(meeting, "treasures", this.session2, this.pGroup , ` #p${this.position} >> strong`, html, dateString);
+            
+            this.position = 10;
+            await this.newDesignation(meeting, "gems", this.session2, this.pGroup , ` #p${this.position} > strong`, html, dateString);
+            this.position = this.position + 5;
+            await this.newDesignation(meeting, "bibleReading", this.session2, this.pGroup , ` #p${this.position} > strong`, html, dateString);
+            
+            await this.newDesignation(meeting, "ministryTitle",this.session3 ,"", ` #p${this.position}`, html, dateString);
+            for(let i = 0 ; i <= 10; i++){
+              await this.newDesignation(meeting, "p",this.session3 , this.pGroup,` #p${this.position} > strong`, html, dateString);
+            }
+            
+            await this.newDesignation(meeting, "christiansTitle", this.session4 , "", ` #p${this.position} > strong`, html, dateString);
+            await this.newDesignation(meeting, "song", this.session4, this.pGroup , ` #p${this.position}`, html, dateString);
 
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "initialSong", session1 + pGroup + "#p3.su", html, dateString));
-          
-          let session2= "#section2 "
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "treasuresTitle", session2 + "#p5 > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "treasures", session2 + pGroup +"#p6.su > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "gems", session2 + pGroup + "#p10.su > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "bibleReading", session2 + pGroup + "#p15.su > strong", html, dateString));
-          
-          let session3= "#section3 "
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "ministryTitle", session3 + "#p16", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session3 + pGroup + "#p17 > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session3 + pGroup + "#p18 > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session3 + pGroup + "#p19 > strong", html, dateString));
-          
-          let session4= "#section4 "
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "christiansTitle", session4 + "#p21", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "song", session4 + pGroup + "#p22", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session4 + pGroup + "#p23 > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session4 + pGroup + "#p24 > strong", html, dateString));
-          
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "p", session4 + pGroup + "#p25 > strong", html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "review ", session4 + pGroup + "#p26 > strong",  html, dateString));
-          ScrapingController.addDesignation(meeting, await ScrapingController.fillDesignation( "finalSong", session4 + pGroup + "#p27", html, dateString));
-          }catch(e) {
+            for(let i = 0 ; i <= 10; i++){
+              while (this.skipNoMarker(this.session4, this.pGroup,  `#p${this.position}`, html)) {
+                this.position++;
+              }
+              if(!await this.newDesignation(meeting, "p", this.session4, this.pGroup , ` #p${this.position}.su > strong`, html, dateString))
+              if(!await this.newDesignation(meeting, "p", this.session4, this.pGroup , ` #p${this.position}.su >> strong`, html, dateString))
+              if(!await this.newDesignation(meeting, "p", this.session4, this.pGroup , ` #p${this.position}.su > strong > em`, html, dateString)){
+              }else{
+                this.position++;
+              }
+            }
+
+            await this.newDesignation(meeting, "review", this.session4, this.pGroup , ` #p${this.position}`, html, dateString);
+            await this.newDesignation(meeting, "finalSong", this.session4, this.pGroup , ` #p${this.position}`, html, dateString);
+
+          } catch (e) {
             console.log(e);
           }
           let meetingDB = await new MeetingDB(meeting);
@@ -108,16 +149,21 @@ export class ScrapingController {
             if (err) {
               // console.log(err);
             } else {
-               meetings.push(meeting);
+              meetings.push(meeting);
             }
           });
 
           momentDate.add(7, "days");
-          stop = true;
         })
         .catch(function(err) {
           console.log(err);
         });
     }
+    return meetings;
   };
+
+
+  private getStringDate(momentDate: moment.Moment) {
+    return momentDate.format("YYYY") + "/" + momentDate.format("MM") + "/" + momentDate.format("DD");
+  }
 }
